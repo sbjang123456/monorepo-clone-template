293e1c3be1d233342d3649dafc5575cf44798c62
JangSuBin <sbjang@storelink.io>
2023-03-23 16:10:53 +0900

    fix: tyepcheck

--- packages/ui-lib/jest.config.js
+++ packages/ui-lib/jest.config.js
@@ -7,15 +7,15 @@ const { getJestCachePath } = require('../../cache.config');
 const packageJson = require('./package.json');
 const { compilerOptions: baseTsConfig } = require('./tsconfig.json');
 
-// ./tsconfig.json 에서 자동적으로 tsconfig 의 경로를 가져옴
-// @link https://kulshekhar.github.io/ts-jest/docs/paths-mapping
-const getTsConfigBasePaths = () => {
-  return baseTsConfig.paths
-    ? pathsToModuleNameMapper(baseTsConfig.paths, {
-        prefix: '<rootDir>/',
-      })
-    : {};
-};
+// // ./tsconfig.json 에서 자동적으로 tsconfig 의 경로를 가져옴
+// // @link https://kulshekhar.github.io/ts-jest/docs/paths-mapping
+// const getTsConfigBasePaths = () => {
+//   return baseTsConfig?.paths
+//     ? pathsToModuleNameMapper(baseTsConfig?.paths, {
+//         prefix: '<rootDir>/',
+//       })
+//     : {};
+// };
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
@@ -36,8 +36,8 @@ const config = {
   testMatch: ['<rootDir>/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   moduleNameMapper: {
     // For @testing-library/react
-    '^@/test-utils$': '<rootDir>/../config/jest/test-utils',
-    ...getTsConfigBasePaths(),
+    // '^@/test-utils$': '<rootDir>/../config/jest/test-utils',
+    // ...getTsConfigBasePaths(),
   },
   // coverage 는 기본적으로 false 으로 설정하고, collect-coverage 를 보기 위해서는 명령어로 실행 (yarn test:unit --collect-coverage=true)
   collectCoverage: false,
