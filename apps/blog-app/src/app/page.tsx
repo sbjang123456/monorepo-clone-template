c7923f1f2f22ac70de135d98c7163af15f88e31b
sbjang <sbjang123456@gmail.com>
2023-10-08 14:32:37 +0900

    feat(blog-app): [#114] add Hero component

--- apps/blog-app/src/app/page.tsx
+++ apps/blog-app/src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import Hero from 'components/Hero';
 
 export default function Home() {
-  return <main className="bg-gray-500">Main</main>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
