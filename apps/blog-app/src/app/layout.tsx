26ce473a9bf7bd3e63649ab076587cce5a2c3a6c
sbjang <sbjang123456@gmail.com>
2023-10-08 17:26:29 +0900

    fix(blog-app): fix import order root layout

--- apps/blog-app/src/app/layout.tsx
+++ apps/blog-app/src/app/layout.tsx
@@ -1,8 +1,8 @@
-import Header from 'components/Header';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import Footer from 'components/Footer';
+import Header from 'components/Header';
 
 const inter = Open_Sans({ subsets: ['latin'] });
 
