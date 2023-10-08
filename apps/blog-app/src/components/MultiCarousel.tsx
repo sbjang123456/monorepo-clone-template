a601fb9c1f7fc7226d02e30c6bd7788ebe9f9523
sbjang <sbjang123456@gmail.com>
2023-10-08 16:44:11 +0900

    feat(blog-app): [#114] add CarouselPosts

---
+++ apps/blog-app/src/components/MultiCarousel.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import type { ReactNode } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function MultiCarousel({ children }: Props) {
+  return (
+    <Carousel infinite autoPlay responsive={responsive} itemClass="m-2">
+      {children}
+    </Carousel>
+  );
+}
--- apps/blog-app/src/components/PostCard.tsx
+++ apps/blog-app/src/components/PostCard.tsx
@@ -10,7 +10,7 @@ export default function PostCard({
 }: Props) {
   return (
     <Link href={path}>
-      <article className="overflow-hidden rounded-md shadow-lg">
+      <article className="overflow-hidden rounded-md shadow-md hover:shadow-xl">
         <Image
           className="w-full"
           src={`/images/posts/${path}.png`}
@@ -19,7 +19,7 @@ export default function PostCard({
           height={200}
         />
         <div className="flex flex-col items-center p-4">
-          <time className="self-end">{date.toString()}</time>
+          <time className="self-end text-gray-700">{date.toString()}</time>
           <h3 className="text-lg font-bold">{title}</h3>
           <p className="w-full truncate text-center">{description}</p>
           <span className="my-2 rounded-lg bg-green-100 px-2 text-sm">
