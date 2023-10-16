df0c316d7c71de5ed33c0135063057e197cb8ab4
sbjang <sbjang123456@gmail.com>
2023-10-16 22:17:38 +0900

    feat(instagram-app): [#122] init instagram-app settings

---
+++ apps/instagram-app/next.config.mjs
@@ -0,0 +1,42 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const trueEnv = ['true', '1', 'yes'];
+
+// const isProd = process.env.NODE_ENV === 'production';
+
+const NEXTJS_IGNORE_ESLINT = trueEnv.includes(
+  process.env?.NEXTJS_IGNORE_ESLINT ?? 'false'
+);
+const NEXTJS_IGNORE_TYPECHECK = trueEnv.includes(
+  process.env?.NEXTJS_IGNORE_TYPECHECK ?? 'false'
+);
+
+/** @type {import('next').NextConfig} */
+let nextConfig = {
+  typescript: {
+    ignoreBuildErrors: !NEXTJS_IGNORE_TYPECHECK,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: !NEXTJS_IGNORE_ESLINT,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+if (process.env.ANALYZE === 'true') {
+  nextConfig = withBundleAnalyzer({
+    enabled: true,
+  })(nextConfig);
+}
+
+export default nextConfig;
